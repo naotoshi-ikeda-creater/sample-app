62d9a30088683756b3a69879f65efa4595b1a9f3
naotoshi-ikeda-creater <nao.0728.2017@gmail.com>
2020-04-18 23:17:47 +0900

    Initial Commit

---
+++ MrUnivercityAppTests/MrUnivercityAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  MrUnivercityAppTests.swift
+//  MrUnivercityAppTests
+//
+//  Created by 尚駿 on 2020/04/18.
+//  Copyright © 2020 尚駿. All rights reserved.
+//
+
+import XCTest
+@testable import MrUnivercityApp
+
+class MrUnivercityAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
